3037b3dc8bfb6811db2c1847cc8f05135eb4dec2
y35634db <daniel.biju@student.manchester.ac.uk>
2023-07-11 12:56:22 +0100

    something went wrong when testing phone. re-wrote some version number

--- kumon-marker-mobile/kumon-marker/kmarker/App.js
+++ kumon-marker-mobile/kumon-marker/kmarker/App.js
@@ -1,7 +1,15 @@
-import React from "react";
-import { SafeAreaView,StatusBar,Text,View, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView,StatusBar,Text,View, StyleSheet, Image } from "react-native";
+//import { launchImageLibrary } from "react-native-image-picker";
 
 const App = () =>{
+
+
+
+
+
+
+    //my app visuals
 
     return (
         <SafeAreaView style={styles.background}>
@@ -28,11 +36,13 @@ const App = () =>{
 const styles = StyleSheet.create({
 
     desc: {
+        color:"#F0FFF0",
         textAlign: 'center',
         marginTop:25
     },
     titleText:{
         fontSize:30,
+        color:"#F0FFF0",
         fontWeight:"bold",
         textAlign: 'center',
         
