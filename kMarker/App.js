dea0a7221655e52bdb3bfe4d3d59270f374785d6
y35634db <daniel.biju@student.manchester.ac.uk>
2023-07-17 23:40:33 +0100

    added a button that launches a camera. need it to now stores that image for OCR

--- kMarker/App.js
+++ kMarker/App.js
@@ -1,49 +1,46 @@
 import React, { useEffect, useState } from "react";
-import { SafeAreaView,StatusBar,Text,View, StyleSheet, Image } from "react-native";
+import { SafeAreaView,StatusBar,Text,View, StyleSheet ,Image,Pressable } from "react-native";
 
 import { launchCamera } from "react-native-image-picker";
-import { RNCamera } from 'react-native-camera';
+
+
+
+
+
+
 const App = () =>{
 
     const options = {
         maxWidth: 2000,
-        maxHeight: 2000
+        maxHeight: 2000,
+        cameraType: "front"
       };
 
-
     const [img,setImg] = useState(null);
-
+/*
     useEffect( () => {
-
         launchCamera(options,setImg);
-
-
-
     },[]);
-
-
-
-
+*/
     //my app visuals
-
     return (
         <SafeAreaView style={styles.background}>
-            <StatusBar>
-            </StatusBar>            
             <View style= {{marginTop:50}}>
-
-
-
                 <Text style= {styles.titleText} >
                     kMarker
                 </Text >
-
                 <Text style = { styles.desc}>
-                   Designed to make marking kumon maths easier cumn
-                </Text>
-               
-                
-                
+                marking maths quicker
+                </Text> 
+
+            <Pressable onPress={ (pressType) =>{
+             
+                launchCamera(options,setImg);
+            } } >
+                <Image tintColor='#A9A9A9' style = {styles.cameraButton}  source={require("./images/camera-button.png")}/> 
+            </Pressable>
+             
+
 
 
             </View>    
@@ -52,17 +49,24 @@ const App = () =>{
 
 }
 
+
+//remember to export this to a different file for a bigger project
+
 const styles = StyleSheet.create({
 
     desc: {
         color:"#F0FFF0",
         textAlign: 'center',
+        alignSelf:'center',
+
         marginTop:25
     },
     titleText:{
         fontSize:30,
         color:"#F0FFF0",
         fontWeight:"bold",
+        alignSelf:'center',
+
         textAlign: 'center',
         
     },
@@ -70,6 +74,18 @@ const styles = StyleSheet.create({
     background:{
         flex:1,
         backgroundColor: "#06191D"
+    },
+
+    cameraButton:{
+        width:"90%",
+        height:undefined,
+        aspectRatio:'1',
+        justifyContent: "center",
+        alignSelf:'center',
+
+        justifyContent: "center",
+        alignItems: "center"
+       
     }
 
 
